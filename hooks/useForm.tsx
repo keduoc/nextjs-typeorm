2a8c4fbc3409941212f198cdd4652ac288bb61d5
keduoc <610471863@qq.com>
2022-08-14 10:56:04 +0800

    处理未登录状态

--- hooks/useForm.tsx
+++ hooks/useForm.tsx
@@ -12,7 +12,7 @@ type useFormOptions<T> = {
   buttons: ReactChild;
   submit: {
     request: (formData: T) => Promise<AxiosResponse<T>>;
-    message: string;
+    success: () => void;
   };
 };
 
@@ -41,19 +41,19 @@ export function useForm<T>(options: useFormOptions<T>) {
   const _onSubmit = useCallback(
     (e) => {
       e.preventDefault();
-      submit.request(formData).then(
-        () => {
-          window.alert(submit.message);
-        },
-        (error) => {
-          if (error.response) {
-            const response: AxiosResponse = error.response;
-            if (response.status === 422) {
-              setErrors(response.data);
-            }
+      submit.request(formData).then(submit.success, (error) => {
+        if (error.response) {
+          const response: AxiosResponse = error.response;
+          if (response.status === 422) {
+            setErrors(response.data);
+          } else if (response.status === 401) {
+            window.alert("请先登录");
+            window.location.href = `/sign_in?returnTo=${encodeURIComponent(
+              window.location.pathname
+            )}`;
           }
         }
-      );
+      });
     },
     [submit, formData]
   );
