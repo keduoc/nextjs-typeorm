2e285ed48447e99b78f368f0fd2ff9201b769c4f
keduoc <610471863@qq.com>
2022-08-18 22:33:18 +0800

    update database orm config

--- lib/getDatabaseConnection.tsx
+++ lib/getDatabaseConnection.tsx
@@ -1,28 +1,31 @@
-import {createConnection, getConnectionManager} from 'typeorm';
-import 'reflect-metadata';
-import {Post} from 'src/entity/Post';
-import {User} from 'src/entity/User';
-import {Comment} from 'src/entity/Comment';
-import config from 'ormconfig.json';
+import { createConnection, getConnectionManager } from "typeorm";
+import "reflect-metadata";
+import { Post } from "src/entity/Post";
+import { User } from "src/entity/User";
+import { Comment } from "src/entity/Comment";
+import config from "ormconfig.json";
 
 const create = async () => {
   // @ts-ignore
   return createConnection({
     ...config,
-    entities: [Post, User, Comment]
+    database:
+      process.env.NODE_ENV === "production"
+        ? "blog_production"
+        : "blog_development",
+    entities: [Post, User, Comment],
   });
 };
 
 const promise = (async function () {
   const manager = getConnectionManager();
-  const current = manager.has('default') && manager.get('default');
-  if (current) {await current.close();}
+  const current = manager.has("default") && manager.get("default");
+  if (current) {
+    await current.close();
+  }
   return create();
 })();
-
 
 export const getDatabaseConnection = async () => {
   return promise;
 };
-
-
