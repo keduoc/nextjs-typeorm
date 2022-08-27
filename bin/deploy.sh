3d4d15e7a4f6e314db1588b71a671cb1c5afe8a9
keduoc <610471863@qq.com>
2022-08-27 21:43:03 +0800

    ignore ormconfig.json

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,15 @@
-echo hi3 > ./temp/tempfile
+echo 'start';
+docker start 9d6 &&
+cd /home/blog/app/ &&
+git pull &&
+yarn install --production=false &&
+yarn build &&
+git apply migrate.patch;
+yarn compile &&
+yarn m:run &&
+git reset --hard HEAD &&
+docker build -t fang/node-web-app . &&
+docker kill app &&
+docker rm app &&
+docker run --name app --network=host -p 3000:3000 -d fang/node-web-app &&
+echo 'OK!'
